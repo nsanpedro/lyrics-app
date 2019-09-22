73e8201dd77f6c274143b82ebb7aaa24db62b08e
nicosp <nicolassanpedro@gmail.com>
2019-09-22 19:51:29 -0300

    axios reqs for lyrics

--- src/components/tracks/Lyrics.js
+++ src/components/tracks/Lyrics.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import axios from 'axios';
+import Spinner from '../layout/Spinner';
 
 class Lyrics extends Component {
     state = {
@@ -6,13 +8,40 @@ class Lyrics extends Component {
         lyrics: {}
     }
 
+    componentDidMount(){
+        this.getLyrics();
+    }
+
+    getLyrics = () => {
+        axios.get(`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${this.props.match.params.id}&apikey=${process.env.REACT_APP_MM_KEY}`)
+            .then(res => {
+                //console.log(res.data);
+                let lyricsData =  res.data.message.body.lyrics.lyric_body;
+                this.setState({lyrics: lyricsData})
+
+                return axios.get(`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.get?track_id=${this.props.match.params.id}&apikey=${process.env.REACT_APP_MM_KEY}`)
+
+            })
+            .then(res => {
+                let trackData = res.data.message.body.track
+                this.setState({track: trackData})
+            })
+            .catch(err => console.log(err))
+    }
+
 
     render() {
-        return (
-            <div>
-                <h1>Lyrics</h1>
-            </div>
-        )
+        const { track, lyrics } = this.state;
+        if(
+            track === undefined || 
+            lyrics === undefined || 
+            Object.keys(track).length === 0 || 
+            Object.keys(lyrics).length === 0
+        ) {
+            return <Spinner />
+        } else {
+            return <h1>Data Returned</h1>
+        }   
     }
 }
 
