4a20439cde1ffa68c42d213677358aee2e9580a4
nicosp <nicolassanpedro@gmail.com>
2019-09-22 20:25:39 -0300

    track & artist name card

--- src/components/tracks/Lyrics.js
+++ src/components/tracks/Lyrics.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import axios from 'axios';
 import Spinner from '../layout/Spinner';
+import { Link } from 'react-router-dom';
 
 class Lyrics extends Component {
     state = {
@@ -15,15 +16,14 @@ class Lyrics extends Component {
     getLyrics = () => {
         axios.get(`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${this.props.match.params.id}&apikey=${process.env.REACT_APP_MM_KEY}`)
             .then(res => {
-                //console.log(res.data);
-                let lyricsData =  res.data.message.body.lyrics.lyric_body;
+                let lyricsData =  res.data.message.body.lyrics.lyrics_body;
                 this.setState({lyrics: lyricsData})
 
                 return axios.get(`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.get?track_id=${this.props.match.params.id}&apikey=${process.env.REACT_APP_MM_KEY}`)
 
             })
             .then(res => {
-                let trackData = res.data.message.body.track
+                let trackData = res.data.message.body.track;
                 this.setState({track: trackData})
             })
             .catch(err => console.log(err))
@@ -38,9 +38,20 @@ class Lyrics extends Component {
             Object.keys(track).length === 0 || 
             Object.keys(lyrics).length === 0
         ) {
-            return <Spinner />
+            return <Spinner />;
         } else {
-            return <h1>Data Returned</h1>
+            return (
+                <React.Fragment>
+                    <Link to="/" className="btn btn-dark btn-sm mb-4">
+                        Go Back
+                    </Link>
+                    <div className="card">
+                        <div className="card-header">
+                            {track.track_name} by <span className="text-secondary">{track.artist_name}</span>
+                        </div>
+                    </div>
+                </React.Fragment>
+            )
         }   
     }
 }
